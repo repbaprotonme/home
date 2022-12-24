0d71d52a1cb97bd4d42b6710352764f62c6407de
Tom Brinkman <reportbase@gmail.com>
2022-12-24 00:19:57 -0500

    na

--- compile/resize.sh
+++ compile/resize.sh
@@ -5,10 +5,10 @@
 #for image in *.jpg; do convert $image -sampling-factor 4:2:0 -strip -quality 80 -interlace JPEG -colorspace RGB $image ; done
 
 #for image in B*LD*.jpg; do convert $image -quality 80 $image; done
-for image in *.jpg; do convert $image -resize x820 $image; done
+#for image in *.jpg; do convert $image -resize x820 $image; done
 #convert KING.0004.jpg -resize x3240 KING.0003.jpg
 
-#for image in *.jpg; do convert $image -quality 80 $image; done
+for image in *.jpg; do convert $image -quality 80 $image; done
 #for image in *.jpg; do convert $image -quality 85 $image; done
 
 #convert tall.0004.jpg -quality 80 a.jpg
