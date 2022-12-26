62b315f34738413a360b429a9c7ab4360ddbcc2a
Tom Brinkman <reportbase@gmail.com>
2022-12-26 01:37:51 -0500

    na

--- repba.js
+++ repba.js
@@ -500,8 +500,8 @@ function drawslices()
             context.visibles++
         }
 
-        context.drawImage(slice.canvas, 0, 0, context.colwidth, rect.height,
-              xn+sn, 0, x1-(xn+sn),  rect.height);
+//        context.drawImage(slice.canvas, 0, 0, context.colwidth, rect.height,
+  //            xn+sn, 0, x1-(xn+sn),  rect.height);
 
         context.slicescount++;
         context.restore();
