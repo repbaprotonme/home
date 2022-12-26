654fa38a3406ee7b841960b477cdc0fb3522e57c
Tom Brinkman <reportbase@gmail.com>
2022-12-26 00:49:04 -0500

    na

--- repba.js
+++ repba.js
@@ -370,7 +370,7 @@ var guidelst =
 
 var debuglst =
     [
-      'black', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
+      'green', 'blue', 'orange', 'purple', 'brown',
       '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
       '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
       '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
