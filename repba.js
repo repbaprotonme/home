00b8d1559e5769852903d2f12a1bdae6c0c423e2
Tom Brinkman <reportbase@gmail.com>
2022-12-28 14:07:57 -0500

    na

--- repba.js
+++ repba.js
@@ -434,8 +434,8 @@ function drawslices()
         if (!slice)
             break;
 
-        var r = calculateAspectRatioFit(context.colwidth, rect.height, rect.width, rect.height);
-        var xt = -rect.width/2;
+        //var r = calculateAspectRatioFit(context.colwidth, rect.height, rect.width, rect.height);
+        var xt = 0;//-rect.width/2;
         context.save();
         context.translate(xt, 0);
         context.shadowOffsetX = 0;
@@ -455,12 +455,10 @@ function drawslices()
             var b = Math.tan(j*VIRTCONST);
             var bx2 = Math.berp(-1, 1, b) * context.virtualpinch - context.virtualeft;
             var stretchwidth = bx2-bx+1;
-            var xx = bx+r.x;
-            var xxx = bx+r.x-width/2;
+            var xx = bx;//+r.x;
             slice.stretchwidth = stretchwidth;
             slice.bx = bx;
             slice.xx = xx;
-            slice.xxx = xxx;
             if (m == 1)
             {
                 x1 = slice.xx;
