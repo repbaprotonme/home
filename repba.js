3a2f031db3ccbf95ae9e17c80bacf533cbc2cb20
Tom Brinkman <reportbase@gmail.com>
2022-12-28 11:57:41 -0500

    na

--- repba.js
+++ repba.js
@@ -494,7 +494,7 @@ function drawslices()
 
         var x = xn+sn;
         var w = x1-x;
-        if (x+w > 0)// && x < width)
+        if (x+w > 0 || x > 2000)// && x < width)
         {
             context.visibles++
             context.drawImage(slice.canvas, 0, 0, context.colwidth, rect.height,
