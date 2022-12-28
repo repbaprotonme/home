f2c490de28cbce4605ebf91f3838d98e0222e9cf
Tom Brinkman <reportbase@gmail.com>
2022-12-28 15:58:07 -0500

    na

--- repba.js
+++ repba.js
@@ -2846,6 +2846,7 @@ function resetcanvas()
     let slicelst = [];
     for (let n = 399; n >= 1; n=n-1)
         slicelst.push({slices: n*3, delay: SLICERADIUS/n});
+    var j = context.virtualwidth/rect.width;
     context.slicewidth = context.virtualwidth/30;//galleryobj.virtualcolumns;
     var slices = 0;
     for (; slices < slicelst.length; ++slices)
