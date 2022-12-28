9d2c9166d1232973b651ab5191ca30254dedae46
Tom Brinkman <reportbase@gmail.com>
2022-12-28 09:42:18 -0500

    na

--- repba.js
+++ repba.js
@@ -472,7 +472,7 @@ function drawslices()
                 sn = stretchwidth;
             }
 
-            if (bx >= width || bx2 < 0 || slice.xx > width)
+            if (bx >= width || bx2 < 0 || (slice.xx + stretchwidth) < 0 || slice.xx > width)
             {
                 bx = bx2;
                 continue;
