5e11ade6b08f52ea7edc67722ccb5cd0dea0eab3
Tom Brinkman <reportbase@gmail.com>
2022-12-28 15:37:15 -0500

    na

--- repba.js
+++ repba.js
@@ -2846,7 +2846,8 @@ function resetcanvas()
     let slicelst = [];
     for (let n = 399; n >= 1; n=n-1)
         slicelst.push({slices: n*3, delay: SLICERADIUS/n});
-    context.slicewidth = context.virtualwidth/galleryobj.virtualcolumns;
+    context.virtualcolumns = context.virtualwidth/rect.width;
+    context.slicewidth = context.virtualwidth/virtualcolumns;
     var slices = 0;
     for (; slices < slicelst.length; ++slices)
     {
@@ -2942,22 +2943,6 @@ var templatelst =
         thumbpos.enabled = 0;
         galleryobj.slidetop = 24;
         galleryobj.slidefactor = 36*2;
-        galleryobj.virtualcolumns = 4;
-        loomobj.split(0, "0-25", loomobj.length());
-        poomobj.split(0, "0-25", poomobj.length());
-        traitobj.split(100, "0.1-1.0", traitobj.length());
-        scapeobj.split(100, "0.1-1.0", scapeobj.length());
-        speedxobj.split(2, "1-20", speedxobj.length());
-        speedyobj.split(2, "1-20", speedyobj.length());
-    }
-},
-{
-    name: "ULTRAWIDE",
-    init: function ()
-    {
-        galleryobj.slidetop = 36;
-        galleryobj.slidefactor = 36*8;
-        galleryobj.virtualcolumns = 4;
         loomobj.split(0, "0-25", loomobj.length());
         poomobj.split(0, "0-25", poomobj.length());
         traitobj.split(100, "0.1-1.0", traitobj.length());
@@ -2973,7 +2958,6 @@ var templatelst =
         footobj.show = 1;
         galleryobj.slidetop = 24;
         galleryobj.slidefactor = 36*3;
-        galleryobj.virtualcolumns = 4;
         loomobj.split(0, "30-80", loomobj.length());
         poomobj.split(0, "0-80", poomobj.length());
         traitobj.split(100, "0.1-1.0", traitobj.length());
@@ -3107,8 +3091,6 @@ fetch(path)
             galleryobj.quality = 75;
         if (typeof galleryobj.galleryobj  === "undefined")
             galleryobj.maxmegapix = 9000000;
-        if (typeof galleryobj.virtualcolumns  === "undefined")
-            galleryobj.virtualcolumns = 6;
 
         photo.help = new Image();
         photo.help.src = "https://reportbase.com/image/HELP"+'/w='+360;
@@ -3248,19 +3230,6 @@ fetch(path)
         }});
 
         slices.data.push({title:"Refresh", path: "REFRESH", func: function(){location.reload();}})
-/*
-        slices.data.push({title:"Slidefactor (-/+)", path: "", func: function(rect, x, y)
-        {
-            galleryobj.slidefactor += (x<rect.width/2)?-36:36;
-            contextobj.reset();
-        }})
-
-        slices.data.push({title:"Slices (-/+)", path: "", func: function(rect, x, y)
-        {
-            galleryobj.virtualcolumns += (x<rect.width/2)?-1:1;
-            contextobj.reset();
-        }})
-*/
         slices.data.push({title:"Debug", path: "DEBUG", func: function(rect, x, y)
         {
             headobj.enabled = 1;
