5cfea2226f97d55166314140346a796d7c4a8ba6
Tom Brinkman <reportbase@gmail.com>
2022-12-30 21:10:06 -0500

    na

--- repba.js
+++ repba.js
@@ -1127,7 +1127,7 @@ CanvasRenderingContext2D.prototype.tab = function ()
     context.slidestart = context.timeobj.current();
     context.slidestop = (context.timeobj.length()/context.virtualwidth)*galleryobj.slidetop;
     context.slidereduce = context.slidestop/galleryobj.slidefactor;
-    galleryobj.slidefactor *= 2;
+    galleryobj.slidefactor *= 4;
     clearInterval(context.timemain);
     context.timemain = setInterval(function () { drawslices() }, TIMEMAIN);
 }
