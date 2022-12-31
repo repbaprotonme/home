f2921365ffad75daf716c54d4819ddb78cd8b49e
Tom Brinkman <reportbase@gmail.com>
2022-12-30 21:07:41 -0500

    na

--- repba.js
+++ repba.js
@@ -416,7 +416,7 @@ function drawslices()
             {
                 context.slidestop -= context.slidereduce;
                 context.timeobj.rotate(context.autodirect*context.slidestop);
-                galleryobj.slidetop = 36;
+                galleryobj.slidefactor = 36;
             }
             else
             {
@@ -1127,7 +1127,7 @@ CanvasRenderingContext2D.prototype.tab = function ()
     context.slidestart = context.timeobj.current();
     context.slidestop = (context.timeobj.length()/context.virtualwidth)*galleryobj.slidetop;
     context.slidereduce = context.slidestop/galleryobj.slidefactor;
-    galleryobj.slidetop *= 2;
+    galleryobj.slidefactor *= 2;
     clearInterval(context.timemain);
     context.timemain = setInterval(function () { drawslices() }, TIMEMAIN);
 }
