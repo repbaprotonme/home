409cae64034561bf16028a9a57d26a1de84d8ae4
Tom Brinkman <reportbase@gmail.com>
2023-01-05 00:03:40 -0500

    na

--- repba.js
+++ repba.js
@@ -894,7 +894,7 @@ var Plus = function (color)
     this.draw = function (context, rect, user, time)
     {
         context.save();
-        context.translate(rect.width/2,rect.height/2);
+        context.translate(rect.width/2-11,rect.height/2-11);
 	    var w = rect.width
         var h = rect.height
         var x = rect.x;
